f0631de0d6c24a324421b39b1adf54e0a480b891
mht-hayate-kakizaki <h.kakizaki@mht-code.com>
2025-01-04 19:14:57 +0900

    apiディレクトリを追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,14 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthController;
 
-Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
-Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->middleware('auth:sanctum')
-    ->name('auth.logout');
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-})->name('auth.user');
+})->middleware('auth:sanctum');
