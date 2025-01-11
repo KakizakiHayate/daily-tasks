32a0e05fa54ca402ca5fd613c3e721eea5f3d62f
hayate <hayate.k.0704@gmail.com>
2025-01-12 01:33:22 +0900

    ec2に本番デプロイするために設定を一時的に変更

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://daily-tasks-app.hyt-apps.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
